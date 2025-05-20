aa2312bad768202f18bafc8b6b75653c8072d6f1
Nikhil Kumar <92905941+Nick1200000@users.noreply.github.com>
2025-05-20 05:38:06 +0000

    Day2 of Learning FastAPI

---
+++ Day2/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI 
+import json
+import os
+
+app = FastAPI()
+
+def load_data():
+    base_path = os.path.dirname(__file__)
+    file_path = os.path.join(base_path, "patients.json")
+    with open(file_path, "r") as f:
+        data = json.load(f)
+    
+    return data
+
+@app.get("/")
+def hello():
+    return {"message": "Patient Management System API"}
+
+@app.get("/about")
+def about():
+    return {"message": "This is a Patient Management System API built with FastAPI."}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
+
